f1fbd052accde6a11c962701bb24134cdcd3a8b7
Intern at PrettyNeat <intern@prettyneat.io>
2022-08-31 12:22:51 +0200

    first fetch

--- backend/backend/Controllers/Test.cs
+++ backend/backend/Controllers/Test.cs
@@ -1,6 +1,38 @@
-﻿namespace backend.Controllers
+﻿using System.Management.Automation;
+using System.Diagnostics;
+using System.Reflection.PortableExecutable;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PowershellShowcase;
+using System.Collections.Generic;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+using System.Diagnostics.Metrics;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("/adduser")]
+
+public class PowerShellScriptController : ControllerBase
+
 {
-    public class Test
+    //get member of user
+    [HttpGet("{id}")]
+    public string Get(string id)
     {
+       
+        string url = "http://localhost:3000/Emplo"
+        return "ciao";
+
+
     }
+
+
+
 }
+
+
+
+
+
+
