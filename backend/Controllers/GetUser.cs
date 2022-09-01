11a29f99b871e03fdfde03be2de232fa12dde715
andreaguli14 <andrea.gulisano1@live.it>
2022-09-01 09:37:17 +0200

    change backend

---
+++ backend/Controllers/GetUser.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Reflection.PortableExecutable;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("/user")]
+
+public class GetUser : ControllerBase
+
+{
+    //get member of user
+    [HttpGet("{id}")]
+    public string Get(string id)
+    {
+        //string z="Get-Aduser -identity internpn";
+        string command = "/c C:\\Users\\internpn\\Desktop\\employee_request_server-main\\powershell_script\\getuser.ps";
+        System.Diagnostics.Process si = new System.Diagnostics.Process();
+        si.StartInfo.WorkingDirectory = "c:\\";
+        si.StartInfo.UseShellExecute = false;
+        si.StartInfo.FileName = "powershell.exe";
+        si.StartInfo.Arguments = command;
+        si.StartInfo.CreateNoWindow = true;
+        si.StartInfo.RedirectStandardInput = true;
+        si.StartInfo.RedirectStandardOutput = true;
+        si.StartInfo.RedirectStandardError = true;
+        si.Start();
+        string output = si.StandardOutput.ReadToEnd();
+        si.Close();
+
+        return output;
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
